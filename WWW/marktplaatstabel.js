539516936a377063542c21a01eebe2389fe10a5c
Ivo van Kamp <vankamp@pathlite.com>
2015-06-11 15:34:14 -0700

    Marktplaatstabel 2.4.4
    Picture upload fix OS X

--- WWW/marktplaatstabel.js
+++ WWW/marktplaatstabel.js
@@ -15,7 +15,7 @@ var PhotoUploader = require('photo-uploader');
 
 var settings = {
     title        : "Marktplaatstabel",
-    version      : "2.4.2",
+    version      : "2.4.4",
     proxyURL     : "http://www..marktplaatstabel.services",
     sslProxyURL  : "http://ssl.marktplaatstabel.services",
     proxyTarget  : "www.marktplaats.nl",
